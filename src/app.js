4f4e2f911c54455c0fd0873dc5799d593474609f
Jay <eng22cse001@gmail.com>
2024-05-07 21:21:09 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 // Progression 1: Create special addition
 
+function specialAddition (numberOne,numberTwo)
+{
+    if (numberOne < 0 || numberTwo < 0) {
+        return 'Negative numbers are not allowed'
+    }
+    return numberOne - numberTwo;
+}
 
 // function specialAddition(){}
 
 // Progression 2: Create simple division
+function simpleDivision(numberOne,numberTwo) {
+   
+    if(numberTwo > numberOne) {
+        return 'Cannot divide a smaller number by a larger number'
+    }
+    else if (numberTwo < 0 || numberOne < 0) {
+        return 'Negative numbers are not allowed'
+
+    }
+    else if(numberTwo==0) {
+        return 'Not a number'
+    }
+    return numberOne / numberTwo;
+}
 
 // Progression 3: Create the special calculator
+
+function specialCalculator(numberOne, numberTwo, opearation) {
+    return opearation(numberOne, numberTwo);
+  }
